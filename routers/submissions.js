7bec4cdd2f60e80fdfcffdd0f08f71770e9bc24d
antoniusdr <antoniusr3101@gmail.com>
2021-06-18 17:06:11 +0200

    added POST route for new submissions

--- routers/submissions.js
+++ routers/submissions.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
-// const auth = require ("../auth/middleware")
+const auth = require("../auth/middleware");
 const Submissions = require("../models").submission;
+const User = require("../models").user;
 
 const router = new Router();
 
@@ -17,6 +18,34 @@ router.get("/", async (req, res, next) => {
   } catch (error) {
     console.log(error);
     return res.status(400).send({ message: "somethin went wrong lol sorry" });
+  }
+});
+
+router.post("/:id", auth, async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const user = await User.findByPk(req.params.id);
+
+    if (user === null) {
+      return res.status(404).send({ message: "This user does not exist" });
+    }
+
+    if (!userId === req.user.id) {
+      return res.status(403).send({ message: "Not authorized to create" });
+    }
+    const { contestId, songDescription, soundcloudUrl } = req.body;
+    const submission = await Submissions.create({
+      userId,
+      contestId,
+      songDescription,
+      soundcloudUrl,
+    });
+    return res.status(201).send({ message: "Submission created", submission });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({
+      message: "somethin went wrong in the backend reach out to a mod sorry...",
+    });
   }
 });
 
